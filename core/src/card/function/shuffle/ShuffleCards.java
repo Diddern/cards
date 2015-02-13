3d5f3ec1c9a6d8e95e6ebeb89aff7c53dffd4af5
Espen Johnsen <espen.john@gmail.com>
2015-02-14 00:04:49 +0100

    startet med shuffle klassen
    ingenting viktig lagt til, men laget det

---
+++ core/src/card/function/shuffle/ShuffleCards.java
@@ -0,0 +1,8 @@
+package card.function.shuffle;
+
+import java.util.Random;
+
+public class ShuffleCards {
+	private Random r;
+
+}
--- core/src/card/structure/deck/Card.java
+++ core/src/card/structure/deck/Card.java
@@ -16,5 +16,4 @@ public class Card {
 	public int getNumber() {
 		return number;
 	}
-
 }
