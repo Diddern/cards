63b62853b27f4ca45e4240a539341b3761830bf8
dsa002 <dsa002@student.uib.no>
2015-02-13 14:09:08 +0100

    First project init

---
+++ android/src/com/cards/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cards.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cards.game.CardsMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CardsMain(), config);
+	}
+}
