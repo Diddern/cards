f8689b1daca92d456d0257b4bd465bb93070cdd5
Espen Johnsen <espen.john@gmail.com>
2015-02-14 15:48:17 +0100

    Litt mer ting testvis

--- core/src/test/structure/deck/TestCard.java
+++ core/src/test/structure/deck/TestCard.java
@@ -10,25 +10,43 @@ import org.junit.Test;
 
 import card.structure.deck.Card;
 
+/**
+ * En enkel testklasse. Det er ikke så mye som kan bli testet, fordi denne skal
+ * være abstrakt.
+ * 
+ * @author Espen Johnsen
+ *
+ */
 public class TestCard {
 	private ArrayList<Card> cards = new ArrayList<Card>();
 	private int size = 1000;
 	private int[] numbers = new int[size];
 	Random r = new Random();
 
+	/**
+	 * Ser om get metoden fungerer.
+	 * 
+	 * @author Espen Johnsen
+	 */
 	@Test
 	public void testGetCards() {
 		for (int i = 0; i < size; i++) {
 			assertTrue(cards.get(i).getNumber() == numbers[i]);
-			assertTrue(cards.get(i).getType().equals("bareEtKort"));
+			assertTrue(cards.get(i).getType()
+					.equals("kortnummer" + "" + numbers[i]));
 		}
 	}
 
+	/**
+	 * Setter opp en liste med mange forskjellige kort.
+	 * 
+	 * @author Espen Johnsen
+	 */
 	@Before
 	public void setup() {
 		for (int i = 0; i < size; i++) {
 			numbers[i] = r.nextInt(13 + 1);
-			cards.add(new Card(numbers[i], "bareEtKort"));
+			cards.add(new Card(numbers[i], "kortnummer" + "" + numbers[i]));
 		}
 	}
 }
--- core/src/test/structure/deck/TestCardDeck.java
+++ core/src/test/structure/deck/TestCardDeck.java
@@ -1,11 +1,41 @@
 package test.structure.deck;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import card.structure.deck.CardDeck;
 
 public class TestCardDeck {
 
-	ArrayList<CardDeck> cardDeck = new ArrayList<CardDeck>();
+	private ArrayList<CardDeck> cardDeckWithJoker = new ArrayList<CardDeck>();
+	private ArrayList<CardDeck> cardDeckWithoutJoker = new ArrayList<CardDeck>();
+	private int size = 1000;
+	private Random r = new Random();
+
+	@Before
+	public void setup() {
+		for (int i = 0; i < size; i++) {
+			cardDeckWithJoker.add(new CardDeck(true));
+			cardDeckWithoutJoker.add(new CardDeck(false));
+		}
+	}
+
+	@Test
+	public void testSize() {
+		for (int i = 0; i < size; i++) {
+			assertTrue(cardDeckWithJoker.get(i).getDeckSize() == 54);
+			assertTrue(cardDeckWithoutJoker.get(i).getDeckSize() == 52);
+		}
+	}
+
+	@Test
+	public void testStruckture() {
+
+	}
 
 }
