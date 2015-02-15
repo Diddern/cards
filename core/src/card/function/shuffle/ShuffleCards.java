bf77fb28f0fdfcaee7cf9ffb733e96fe3685a17c
Espen Johnsen <espen.john@gmail.com>
2015-02-15 16:53:14 +0100

    Har laget shuffle test som tester margin.
    Med den nye klassen så vet eg at shuffle er feil

--- core/src/card/function/shuffle/ShuffleCards.java
+++ core/src/card/function/shuffle/ShuffleCards.java
@@ -6,7 +6,7 @@ import card.structure.deck.Card;
 import card.structure.deck.CardDeck;
 
 public class ShuffleCards {
-	private Random r;
+	private Random r1, r2;
 
 	/**
 	 * Tror ikke det går ann å teste denne skikkelig. Det e vel det som e
@@ -14,13 +14,21 @@ public class ShuffleCards {
 	 * 
 	 * @param deck
 	 */
-	public void shuffleCards(CardDeck deck) {
-		r = new Random();
-		int size = deck.getDeckSize();
+	public CardDeck shuffleCards(CardDeck deck) {
+		r1 = new Random(deck.getDeckSize());
+		r2 = new Random(deck.getDeckSize() - 1);
+
 		for (int i = 0; i < 1000; i++) {
-			Card card = deck.takeCard(r.nextInt(size));
-			deck.putCardBackInn(r.nextInt(size), card);
+			int random1 = r1.nextInt(deck.getDeckSize());
+			Card card = deck.takeCard(random1);
+
+			int random2 = r2.nextInt(deck.getDeckSize());
+			if (random1 == random2) {
+				System.out.println("feil");
+			}
+			deck.putCardBackInn(random2, card);
 		}
+		return deck;
 	}
 
 }
--- core/src/card/structure/deck/Card.java
+++ core/src/card/structure/deck/Card.java
@@ -16,4 +16,19 @@ public class Card {
 	public int getNumber() {
 		return number;
 	}
+
+	/**
+	 * checks if the card given has the same number and is the same type
+	 * 
+	 * @param card
+	 *            the card you want to send
+	 * @return true if it is equal
+	 */
+	public boolean equalsTo(Card card) {
+		if (card.getNumber() == number) {
+			if (type.equals(card.getType()))
+				return true;
+		}
+		return false;
+	}
 }
